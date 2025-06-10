e4517c5f47643464418aa3af46d826b14472219a
Maya Thompson <mayaandromedathompson@gmail.com>
2025-06-10 11:06:17 -0500

    workbook exercise 8-2-3

--- NorthwindTraders/src/main/java/com/pluralsight/Main.java
+++ NorthwindTraders/src/main/java/com/pluralsight/Main.java
@@ -1,47 +1,89 @@
 package com.pluralsight;
 
 import com.pluralsight.utils.IOUtils;
-
 import java.sql.*;
 
 public class Main {
+    static String user = IOUtils.messageAndResponse("User: ");
+    static String password = IOUtils.messageAndResponse("Pass: ");
 
     public static void main(String[] args) {
 
-        try {
-            mySqlQuery();
+        boolean keepMenuRunning = true;
+
+        while (keepMenuRunning) {
+            int choice = mainMenuUI();
+
+            switch (choice) {
+                case 1 -> displayAllProducts();
+                case 2 -> displayAllCustomers();
+                case 0 -> keepMenuRunning = false;
+                default -> System.out.println("Please choose an available option.");
+            }
+            IOUtils.pauseReturn();
+        }
+    }
+
+    private static void displayAllCustomers() {
+        String url = "jdbc:mysql://localhost:3306/northwind";
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM customers ORDER BY Country;");
+
+            ResultSet results = statement.executeQuery();
+            StringBuilder stringOut = new StringBuilder();
+            while (results.next()) {
+                String contactName = results.getString("ContactName");
+                String companyName = results.getString("CompanyName");
+                String city = results.getString("City");
+                String country = results.getString("Country");
+                String phoneNumber = results.getString("Phone");
+
+                String singleResult = String.format("Contact Name: %s\nCompany Name: %s\nCity: %s\nCountry: %s\nPhone Number: %s", contactName, companyName, city, country, phoneNumber);
+                stringOut.append(singleResult);
+                stringOut.append("\n\n--------------------\n\n");
+
+            }
+            System.out.println(stringOut);
         } catch (SQLException | ClassNotFoundException e) {
             System.out.println("There was a Sql issue");
         }
     }
 
-    private static void mySqlQuery() throws SQLException, ClassNotFoundException {
+    private static void displayAllProducts() {
         String url = "jdbc:mysql://localhost:3306/northwind";
-        String user = IOUtils.messageAndResponse("User: ");
-        String password = IOUtils.messageAndResponse("Pass: ");
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection = DriverManager.getConnection(url, user, password);
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
-        Statement statement = connection.createStatement();
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM products ORDER BY ProductName;");
 
-        String query = "SELECT * FROM products ORDER BY ProductID;";
+            ResultSet results = statement.executeQuery();
+            StringBuilder stringOut = new StringBuilder();
+            while (results.next()) {
+                int productId = results.getInt("ProductID");
+                String productName = results.getString("ProductName");
+                double unitPrice = results.getDouble("UnitPrice");
+                int unitsInStock = results.getInt("UnitsInStock");
 
-        ResultSet results = statement.executeQuery(query);
-        StringBuilder stringOut = new StringBuilder();
-        while (results.next()) {
-            int productId = results.getInt("ProductID");
-            String productName = results.getString("ProductName");
-            double unitPrice = results.getDouble("UnitPrice");
-            int unitsInStock = results.getInt("UnitsInStock");
+                String singleResult = String.format("Product ID: %d\nProduct Name: %s\nPrice: %.2f\nUnits in Stock: %d", productId, productName, unitPrice, unitsInStock);
+                stringOut.append(singleResult);
+                stringOut.append("\n\n--------------------\n\n");
 
-            String singleResult = String.format("Product ID: %d\nProduct Name: %s\nPrice: %.2f\nUnits in Stock: %d", productId, productName, unitPrice, unitsInStock);
-            stringOut.append(singleResult);
-            stringOut.append("\n\n--------------------\n\n");
-
+            }
+            System.out.println(stringOut);
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("There was a Sql issue");
         }
-        System.out.println(stringOut);
-        connection.close();
     }
 
+    public static int mainMenuUI() {
+        System.out.println("""
+                What do you want to do?
+                1) Display all products
+                2) Display all customers
+                0) Exit""");
+        return IOUtils.messageAndResponseInt("Choose an option: ");
+    }
 
 }
