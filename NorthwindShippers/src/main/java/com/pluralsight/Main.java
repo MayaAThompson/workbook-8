2b274163c4154b91523e52f6c962316c1936c61b
Maya Thompson <mayaandromedathompson@gmail.com>
2025-06-12 09:59:35 -0500

    workbook 8-4-1 exercise added functionality to all methods

--- NorthwindShippers/src/main/java/com/pluralsight/Main.java
+++ NorthwindShippers/src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,28 @@
 package com.pluralsight;
 
 public class Main {
+
+    public static void main(String[] args) {
+        ShipperDao dataManager = new ShipperDao();
+
+        dataManager.displayShipperList(dataManager.getAllShippers());
+
+        System.out.println("Add a new shipper");
+        String companyName = "TransUS Freight";
+        String phoneNumber = "214-1233-4567";
+        dataManager.insertNewShipper(companyName, phoneNumber);
+
+        dataManager.displayShipperList(dataManager.getAllShippers());
+
+        System.out.println("Update the phone number");
+        dataManager.updateShipperPhoneNumber(7, "214-098-7654");
+
+        dataManager.displayShipperList(dataManager.getAllShippers());
+
+        System.out.println("Delete a shipper");
+        dataManager.deleteShipper(7);
+
+        dataManager.displayShipperList(dataManager.getAllShippers());
+
+    }
 }
--- NorthwindShippers/src/main/java/com/pluralsight/ShipperDao.java
+++ NorthwindShippers/src/main/java/com/pluralsight/ShipperDao.java
@@ -1,7 +1,8 @@
 package com.pluralsight;
 
 import org.apache.commons.dbcp2.BasicDataSource;
-
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ShipperDao {
@@ -18,11 +19,86 @@ public class ShipperDao {
         this.dataSource.setPassword(PASSWORD);
     }
 
-    public boolean insertNewShipper() {
-        throw new RuntimeException("Not yet implemented");
+    public void insertNewShipper(String companyName, String phone) {
+        try (Connection connection = dataSource.getConnection()) {
+            String insert = """
+                    insert into shippers (CompanyName, Phone)
+                    	values (?, ?);""";
+            PreparedStatement statement = connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, companyName);
+            statement.setString(2, phone);
+            int rowsUpdated = statement.executeUpdate();
+
+            ResultSet keys = statement.getGeneratedKeys();
+            System.out.println(rowsUpdated + "row(s) updated");
+            while (keys.next())
+                System.out.println("New Shipper Key: " + keys.getLong(1));
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<Shipper> getAllShippers() {
-        throw new RuntimeException("Not yet implemented");
+        List<Shipper> shippers = new ArrayList<>();
+        try (Connection connection = dataSource.getConnection()) {
+            String query = """
+                    SELECT *
+                    FROM shippers;""";
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet results = statement.executeQuery();
+
+            while (results.next()) {
+                int id = results.getInt("ShipperID");
+                String name = results.getString("CompanyName");
+                String phone = results.getString("Phone");
+
+                Shipper shipper = new Shipper(id, name, phone);
+                shippers.add(shipper);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return shippers;
+    }
+
+    public void updateShipperPhoneNumber(int id, String phoneNumber) {
+        int rowsUpdated = 0;
+        try (Connection connection = dataSource.getConnection()) {
+            String update = """
+                    update shippers
+                    	set Phone = ?
+                        where ShipperID = ?""";
+            PreparedStatement statement = connection.prepareStatement(update);
+            statement.setString(1, phoneNumber);
+            statement.setString(2, String.valueOf(id));
+            rowsUpdated = statement.executeUpdate();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println(rowsUpdated + " row(s) updated.");
+    }
+
+    public void deleteShipper (int id) {
+        int rowsUpdated = 0;
+        try (Connection connection = dataSource.getConnection()) {
+            String delete = """
+                    delete from shippers
+                    	where ShipperID = ?;""";
+            PreparedStatement statement = connection.prepareStatement(delete);
+            statement.setString(1, String.valueOf(id));
+            rowsUpdated = statement.executeUpdate();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println(rowsUpdated + "row(s) updated.");
+    }
+
+    public void displayShipperList(List<Shipper> shippers) {
+        for (Shipper shipper : shippers) {
+            System.out.println("\n" + shipper);
+        }
     }
 }
